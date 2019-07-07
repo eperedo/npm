e400e3a68ddfaeaee26011ef4c87ef9bb94c7e6e
Eduardo P. Rivero <jrperedo@gmail.com>
2019-07-07 12:05:34 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -n "$GITHUB_TOKEN" ]; then
+if [ -n "$GH_REGISTRY_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-npm.pkg.github.com}"
@@ -14,7 +14,7 @@ if [ -n "$GITHUB_TOKEN" ]; then
   # fi
 
   # Allow npm.pkg.github.com to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$GITHUB_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$GH_REGISTRY_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
 
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
